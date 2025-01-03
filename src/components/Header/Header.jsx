24573255e547c2a2a4a3b04cc6da1b6b373887f6
Roman <roman.mitsura@mail.ru>
2025-01-03 15:10:59 +0300

    adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
       <Link to={"/"}>
         <span className="bg-blue-500 p-2 hidden sm:block">logo</span>
       </Link>
-      <div className="group flex items-center pl-20 sm:pl-7">
+      <div className="group flex items-center pl-10 sm:pl-7">
         <input
           className="text-white all:unset border border-r-0 border-gray-400 focus:outline-none focus:border-blue-400 bg-neutral-900 px-3 py-2 rounded-l-md"
           type="text"
@@ -41,47 +41,50 @@ export default function Header() {
           </svg>
         </label>
       </div>
-
-      {currentUser ? (
-        <Link
-          to={`/profile/${currentUser.id}`}
-          className="flex items-center gap-2"
-        >
-          <span className="text-white">{currentUser.username}</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-8"
+      <div>
+        {currentUser ? (
+          <Link
+            to={`/profile/${currentUser.id}`}
+            className="flex items-center gap-2"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
-        </Link>
-      ) : (
-        <Link to={"/login"} className="flex items-center gap-2">
-          <span className="text-white">Войти</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-8"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
-        </Link>
-      )}
+            <span className="text-white hidden sm:block">
+              {currentUser.username}
+            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </Link>
+        ) : (
+          <Link to={"/login"} className="flex items-center gap-2">
+            <span className="text-white hidden sm:block">Войти</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
